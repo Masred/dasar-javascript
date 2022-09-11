7a6c2afff6e93319325e9f0e54f6acbd83063085
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2022-09-11 21:22:34 +0700

    adding kuis

---
+++ struktur-data/kuis-object.js
@@ -0,0 +1,36 @@
+/*
+ * TODO
+ * 1. Buatlah variabel dengan nama restaurant yang bertipe object dengan ketentuan berikut:
+ *    - Memiliki properti bernama "name"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti bernama "city"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti "favorite drink"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti "favorite food"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti "isVegan"
+ *       - Bertipe data boolean
+ *       - Bernilai boolean apa pun.
+ *
+ * 2. Buatlah variabel bernama name.
+ *    Kemudian isi dengan nilai name dari properti object restaurant
+ * 3. Buatlah variabel bernama favoriteDrink.
+ *    Kemudian isi dengan nilai "favorite drink" dari properti object restaurant
+ */
+
+// TODO
+let restaurant = {
+  name: "Masred",
+  city: "Birmingham",
+  "favorite drink": "Whisky Irish",
+  "favorite food": "pizza",
+  isVegan: false,
+};
+
+let name = restaurant.name;
+let favoriteDrink = restaurant["favorite drink"];
